29e1e0c3c1564ee19ebbd895dc51f012f1a512b5
Chaerul Umam <chaerulumamb244@gmail.com>
2022-10-30 13:54:25 +0700

    setup route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl text-white font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
