edafdcde496aca1ffef502e0f4e37222d213503f
Chaerul Umam <chaerulumamb244@gmail.com>
2022-10-30 15:51:16 +0700

    row sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import request from "../Request";
 
 const Home = () => {
   return (
     <div>
       <Main />
+      <Row title="Up Coming" fetchURL={request.requestUpcoming} />
+      <Row title="Popular" fetchURL={request.requestPopular} />
+      <Row title="Trending" fetchURL={request.requestTrending} />
+      <Row title="Horror" fetchURL={request.requestHorror} />
+      <Row title="Top Rated" fetchURL={request.requestTopRated} />
     </div>
   );
 };
