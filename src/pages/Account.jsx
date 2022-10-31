461dc808586f9e62a40c28b35b41b679bbf47662
Chaerul Umam <chaerulumamb244@gmail.com>
2022-10-31 22:37:51 +0700

    implement show movies

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import SavedShows from "../components/SavedShows";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/79fe83d4-7ef6-4181-9439-46db72599559/83b58a62-6624-4188-8119-8bdf395dae3f/ID-en-20221017-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+          className="w-full h-[400px] object-cover"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
 };
 
 export default Account;
